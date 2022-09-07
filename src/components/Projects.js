c6bfd6cf7a0bf8a864cc347c637f99a043c4e876
Tanish <tanishchaurasia6@gmail.com>
2022-09-08 00:40:50 +0530

    fix

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -158,7 +158,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} alt="color"></img>
     </section>
   )
 }
